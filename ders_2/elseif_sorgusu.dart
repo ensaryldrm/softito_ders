dfe605442cceaa06d562f17111f13dff920c3890
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-05-08 17:42:41 +0300

    ek sorular eklendi

--- ders_2/elseif_sorgusu.dart
+++ ders_2/elseif_sorgusu.dart
@@ -1,5 +1,7 @@
+import "dart:io";
+
 void main(List<String> args) {
-  int numberOne = 17;
+  /*int numberOne = 17;
   int numberTwo = -55;
   if(numberOne > numberTwo)
   {
@@ -16,4 +18,60 @@ void main(List<String> args) {
     print("$numberOne sayisi $numberTwo sayisina esittir.");
   }
 
+  // Sayinin tek-cift olmasini gosteren program
+  stdout.write("Lutfen bir sayi giriniz: ");
+  int inputNumber = int.parse(stdin.readLineSync()!);
+  if(inputNumber == 0)
+  {
+    print("Girdiginiz $inputNumber sayisinin isareti yoktur..");
+  }
+
+  else if(inputNumber > 0)
+  {
+    print("Girdiginiz $inputNumber sayisinin isareti pozitiftir");
+  }
+
+  else
+  {
+    print("Girdiginiz $inputNumber sayisinin isareti negatiftir");
+  }*/
+
+  // Kullanici kendi islemini secerek matematiksel islem yaptıracak program
+  stdout.write("Lutfen birinci sayiyi giriniz: ");
+  double a = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Lutfen ikinci sayiyi giriniz: ");
+  double b = double.parse(stdin.readLineSync()!);
+
+  stdout.write("--Yapmak istediginiz islemi seciniz--\nToplama icin 1\nCikarma icin 2\nCarpma icin 3\nBolme icin 4\nMod alma icin 5\n:");
+  String? secim = stdin.readLineSync()!;
+
+  if(secim == "1")
+  {
+    print("$a + $b = ${a+b}");
+  }
+
+  else if(secim == "2")
+  {
+    print("$a - $b = ${a-b}");
+  }
+
+  else if(secim == "3")
+  {
+    print("$a * $b = ${a/b}");
+  }
+
+  else if(secim == "4")
+  {
+    print("$a / $b = ${a/b}");
+  }
+  else if(secim == "5")
+  {
+    print("$a % $b = ${a%b}");
+  }
+
+  else
+  {
+    print("Yanlis sayi girdiniz!!");
+  }
 }
