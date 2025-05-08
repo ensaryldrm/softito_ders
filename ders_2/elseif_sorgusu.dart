7724aaa99aba90f7eb584292109fc17138ccf6fd
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-05-08 16:51:34 +0300

    elseif sorgusu eklendi

---
+++ ders_2/elseif_sorgusu.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  int numberOne = 17;
+  int numberTwo = -55;
+  if(numberOne > numberTwo)
+  {
+    print("$numberOne sayisi $numberTwo sayisindan  buyuktur.");
+  }
+
+  else if(numberTwo > numberOne)
+  {
+    print("$numberTwo sayisi $numberOne sayisindan  buyuktur.");
+  }
+
+  else
+  {
+    print("$numberOne sayisi $numberTwo sayisina esittir.");
+  }
+
+}
