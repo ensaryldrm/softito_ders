a169468e2d97f215fa7823ef187d9b3781d4ef8d
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-05-09 14:25:18 +0300

    ders3 klasoru eklendi ve içine döngüler kısmı eklendi.

---
+++ ders_3/donguler.dart
@@ -0,0 +1,33 @@
+void main(List<String> args) {
+  /*for(int i = 0; i<=10; i++) { 1.ORNEK
+    print(i);
+  }
+
+  for(int j = 0; j<11; j++) { 2.ORNEK
+    print(j);
+  }
+
+  for(int k = 0; k <11; k += 1) { 3.ORNEK
+    print(k);
+  }*/
+
+  /* for(int k = 5; k <13; k += 1) { 4.ORNEK
+    print(k);
+  } */
+
+ /* for (var i = 1; i <=5; i++) { 5.ORNEK
+   print("Ensar");
+ } */
+
+  /* for (var i = 1; i <= 5; i++) { 6.ORNEK
+    print("$i. Fenerbahce");
+  } */
+
+ /* for (var i = 0; i <= 23; i+=2) { 7. ORNEK
+   print(i);
+ } */
+
+ /* for (var i = 1; i < 41; i+=2) { 8. ORNEK
+   print(i);
+ } */
+}
