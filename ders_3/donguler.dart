a5951e01327173ef8dab70a140377d4b66750507
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-05-09 15:22:25 +0300

    for döngüleri için örnekler eklendi.

--- ders_3/donguler.dart
+++ ders_3/donguler.dart
@@ -1,33 +1,76 @@
+import "dart:io";
+
 void main(List<String> args) {
-  /*for(int i = 0; i<=10; i++) { 1.ORNEK
+/* for(int i = 0; i<=10; i++) { 1.ORNEK
     print(i);
-  }
+ }
 
-  for(int j = 0; j<11; j++) { 2.ORNEK
+ for(int j = 0; j<11; j++) { 2.ORNEK
     print(j);
+ }
+
+ for(int k = 0; k <11; k += 1) { 3.ORNEK
+    print(k);
+ } 
+
+ for(int k = 5; k <13; k += 1) { 4.ORNEK
+    print(k);
+ }
+
+ for (var i = 1; i <=5; i++) { 5.ORNEK
+   print("Ensar");
+ }
+
+  for (var i = 1; i <= 5; i++) { 6.ORNEK
+    print("$i. Fenerbahce");
   }
 
-  for(int k = 0; k <11; k += 1) { 3.ORNEK
-    print(k);
-  }*/
+ for (var i = 0; i <= 23; i+=2) { 7. ORNEK
+   print(i);
+ }
 
-  /* for(int k = 5; k <13; k += 1) { 4.ORNEK
-    print(k);
-  } */
-
- /* for (var i = 1; i <=5; i++) { 5.ORNEK
-   print("Ensar");
- } */
-
-  /* for (var i = 1; i <= 5; i++) { 6.ORNEK
-    print("$i. Fenerbahce");
-  } */
-
- /* for (var i = 0; i <= 23; i+=2) { 7. ORNEK
+ for (var i = 1; i < 41; i+=2) { 8. ORNEK
    print(i);
  } */
 
- /* for (var i = 1; i < 41; i+=2) { 8. ORNEK
+ /* var bas = 42;  9.ORNEK
+ var bit = 123;
+ for (var i = bas; i < bit; i++) {
+   print(i);
+ } */
+
+ /* stdout.write("Ust limit icin sayi giriniz: "); 10.ORNEK
+ int lastNumber = int.parse(stdin.readLineSync()!);
+ for (var i = 1; i <= lastNumber; i++) {
+   print(i);
+ } */
+
+ /* stdout.write("Baslangic degerini giriniz: "); 11.ORNEK
+ int firstNumber = int.parse(stdin.readLineSync()!);
+ for (var i = firstNumber; i <= 25; i++) {
+  print(i);
+ } */
+
+ /* stdout.write("Baslangic degeri giriniz: "); 12.ORNEK
+ int firstNumber = int.parse(stdin.readLineSync()!);
+
+ stdout.write("Ust limit icin sayi giriniz: ");
+ int lastNumber = int.parse(stdin.readLineSync()!);
+
+ for (var i = firstNumber; i <= lastNumber; i++) {
+   print(i);
+ } */
+
+ /* stdout.write("Baslangic degeri giriniz: "); 13.ORNEK
+ int firstNumber = int.parse(stdin.readLineSync()!);
+
+ stdout.write("Ust limit icin sayi giriniz: ");
+ int lastNumber = int.parse(stdin.readLineSync()!);
+
+ stdout.write("Artis degeri icin sayi giriniz: ");
+ int increment = int.parse(stdin.readLineSync()!);
+
+ for (var i = firstNumber; i <= lastNumber; i += increment) {
    print(i);
  } */
 }
