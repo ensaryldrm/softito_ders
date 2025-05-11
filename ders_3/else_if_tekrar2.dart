76a916a8fe546e7f5f919c418f102221b83f9de2
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-05-11 22:10:47 +0300

    else if için 2. tekrar eklendi.

---
+++ ders_3/else_if_tekrar2.dart
@@ -0,0 +1,20 @@
+import "dart:io";
+
+void main(List<String> args) {
+  int i = 1;
+  int sayac = 0;
+  int toplam = 0;
+  while(i <= 20)
+  {
+    stdout.write("$i. sayiyi giriniz: ");
+    int sayi = int.parse(stdin.readLineSync()!);
+
+    if(sayi >= 0)
+    {
+      sayac++;
+      toplam += sayi;
+    }
+    i++;
+  }
+  print("Girilen pozitif sayilarin ortalamasi: ${toplam / sayac}");
+}
