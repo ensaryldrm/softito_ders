7dd711882806832a737aeeac1d9fc7ff1a5b45d5
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-05-15 12:47:15 +0300

    fonksiyonlar için örnekler eklendi

--- hafta_2/ders_5/fonksiyonlar.dart
+++ hafta_2/ders_5/fonksiyonlar.dart
@@ -1,5 +1,5 @@
 import "dart:io";
-import "dart:math";
+import "dart:core";
 
 void main(List<String> args) {
   /*
@@ -67,14 +67,85 @@ void main(List<String> args) {
   /* 
   sayiYazdir();
   */
+
+  /*
   sayiIste("baslangic");
-  int sayi = int.parse(stdin.readLineSync()!);
-  basDegeri(sayi);
+  int sayi1 = int.parse(stdin.readLineSync()!);
+  basDegeri(sayi1);
+
+  sayiIste("bitis");
+  int sayi2 = int.parse(stdin.readLineSync()!);
+  bitDegeri(sayi2);
+  */
+
+  /* sayiIste("baslangic");
+  int sayi1 = int.parse(stdin.readLineSync()!);
+
+  sayiIste("bitis");
+  int sayi2 = int.parse(stdin.readLineSync()!);
+  basBitDegeri(sayi1, sayi2); */
+  
+ /*  
+ print("Guncel Tarih: ${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}");
+  while(true){
+    sayiIste("dogum");
+    int myBirthday = int.parse(stdin.readLineSync()!);
+    stdout.write("Suanki yasinizi bulmak icin 1 e kendi gireceginiz bir tarih icin 2 ye basiniz: ");
+    String? secim = stdin.readLineSync()!;
+    yasHesaplama(myBirthday, secim!);
+  }
+  */
+
+  genelIslemler();
 }
 
-void sayiIste(String metin){
-  stdout.write("Lutfen $metin sayisini giriniz: ");
+void genelIslemler(){
+ while(true){
+    stdout.write("Lutfen bir sayi giriniz: ");
+    int sayi = int.parse(stdin.readLineSync()!);
+
+    tekMiCiftMi(sayi);
+  }
 }
+
+void tekMiCiftMi(int a){
+  a % 2 == 0 
+      ? print("Girdiginiz $a sayisi cift sayidir!")
+      : print("Girdiginiz $a sayisi tek sayidir!");
+}
+
+/* void sayiIste(String metin){
+  stdout.write("Lutfen $metin sayi giriniz: ");
+} */
+
+/* void yasHesaplama(int dogumTarihi, String selected){
+  int age = 0;
+  if(selected == "1"){
+    int nowYear = DateTime.now().year;
+    age = nowYear - dogumTarihi;
+  }
+  else{
+    stdout.write("Lutfen hesaplatmak istediginiz yili giriniz: ");
+    int myYear = int.parse(stdin.readLineSync()!);
+    age = myYear - dogumTarihi;
+  }
+  print("Girdiginiz yila gore yasiniz $age");
+} */
+
+
+/* //her ikisini de kullanicidan alan program
+void basBitDegeri(int bas, int bit){
+  for(int i = bas; i<=bit;i++){
+    print(i);
+  }
+}
+// baslangic degeri 1 den kullanicinin istedigi sayiya kadar
+void bitDegeri(int bit){
+  for(int i = 1; i<=bit;i++){
+    print(i);
+  }
+}
+
 // baslangic degeri alip 20 ye kadar yazdiran program
 void basDegeri(int bas){
   for(int i = bas; i<=20;i++){
@@ -104,5 +175,5 @@ void sayiTopla(){
   int sayi2 = 7;
   
   print("$sayi1 + $sayi2 = ${sayi1 + sayi2}");
-}
+} */
 
