6479981e5ed653c06da2c464024c06bd3495df78
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-05-15 16:28:57 +0300

    ornek eklendi

---
+++ hafta_2/ders_5/varsayilan_parametre_ornek.dart
@@ -0,0 +1,18 @@
+import "dart:ffi";
+import "dart:io";
+
+void main(List<String> args) {
+  while(true){
+    stdout.write("Lutfen kullanici adinizi girin: ");
+    String? userName = stdin.readLineSync()!;
+    if(userName == ""){
+      kullaniciSelamla();
+    }else{
+      kullaniciSelamla(userName);
+    }
+  }
+}
+
+void kullaniciSelamla([String name = "Yabanci"]){
+  print("Merhaba $name");
+}
