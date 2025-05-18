09d4ed646d8943883f11a450e6daf4352ab7b6d7
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-05-18 12:10:20 +0300

    hafta2 nin ödev1 i tamamlandı

--- hafta2_odevler/odev1.dart
+++ hafta2_odevler/odev1.dart
@@ -1,3 +1,34 @@
+import "dart:io";
+
 void main(List<String> args) {
   // Kullanicidan 3 tane sayi alacaksiniz. Kullanicinin girdigi  bilgilere gore ucgenin cesidini yazacaksınız. (Ornek: 3 - 7 - 9 ise cesit kenar yazacak.)
+
+  stdout.write("1. sayiyi giriniz: ");
+  int sayi1 = int.parse(stdin.readLineSync()!);
+
+  stdout.write("2. sayiyi giriniz: ");
+  int sayi2 = int.parse(stdin.readLineSync()!);
+
+  stdout.write("3. sayiyi giriniz: ");
+  int sayi3 = int.parse(stdin.readLineSync()!);
+
+  if (sayi1 + sayi2 >= sayi3 &&
+      sayi3 + sayi2 >= sayi1 &&
+      sayi3 + sayi1 >= sayi2) {
+    if (sayi1 == sayi3 && sayi1 == sayi2) {
+      print(
+        "Girdiginiz kenarlara gore ($sayi1 - $sayi2 - $sayi3), ucgeniniz eskenardir!",
+      );
+    } else if (sayi1 == sayi2 || sayi1 == sayi3 || sayi2 == sayi3) {
+      print(
+        "Girdiginiz kenarlara gore ($sayi1 - $sayi2 - $sayi3), ucgeniniz ikizkenardir!",
+      );
+    } else {
+      print(
+        "Girdiginiz kenarlara gore ($sayi1 - $sayi2 - $sayi3), ucgeniniz cesitkenardir!",
+      );
+    }
+  } else {
+    print("Girdiginiz kenarlar ile ucgen olusturulamaz!");
+  }
 }
