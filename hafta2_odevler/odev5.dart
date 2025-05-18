6670910586cb8a43d3e03d06696bcd89870b004f
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-05-18 12:28:48 +0300

    hafta2'nin odev5 tamamlandı

--- hafta2_odevler/odev5.dart
+++ hafta2_odevler/odev5.dart
@@ -1,3 +1,11 @@
 void main(List<String> args) {
   // 1 den 20 ye kadar faktoriyeli 1000 den buyuk olanlari ekrana yazdirin.
+  
+  double fakt = 1;
+  for(int i = 1; i<=20; i++){
+    fakt *= i;
+    if(fakt > 1000){
+      print("$i sayisinin faktoriyel degeri: $fakt");
+    }
+  }
 }
